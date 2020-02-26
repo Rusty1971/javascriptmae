749ac547bc17be91385e931132e293795862bbbf
Nicolae1188 <nicolaefrunza88@gmail.com>
2020-02-26 17:02:35 +0000

    adding test javascript

---
+++ test.js
@@ -0,0 +1,10 @@
+// Test Code
+
+let breakfast = "Nothing";
+let lunch = "Meatball Sandwich";
+let dinner = "Chicken Curry";
+console.log(`For breakfast I had ${breakfast}, for lunch I had ${lunch} and my tea was ${dinner}`)
+breakfast = "poridge";
+lunch = "live noodles";
+dinner = "beef curry";
+console.log(`For breakfast I had ${breakfast}, for lunch I had ${lunch} and my tea was ${dinner}`)
